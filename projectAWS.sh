c60a940f4e6b75ce60dc2ee6b88ec2bf34659964
moneyj1 <moneyjerome1@gmail.com>
2024-12-19 11:22:54 -0500

    first change made

---
+++ projectAWS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##################
+#Author: Jerome M. Wachira
+#Date: 11th-jan 2024
+#Version: v1
+#This scripts will report the AWS resource usage
+##################
+
+echo"welcome ; first learn what is a crontab/cronjob aws "
+
+echo "hello commit 1"
